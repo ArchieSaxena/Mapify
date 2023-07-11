7c7f7b85416259a70337c57069b2d33d08334e55
Archie Saxena <saxenaarchie560@gmail.com>
2023-07-11 16:13:38 +0530

    Rendering Work Input form

--- script.js
+++ script.js
@@ -11,6 +11,7 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
+let map,mapEvent;
 if(navigator.geolocation)
 {
     navigator.geolocation.getCurrentPosition(
@@ -21,30 +22,19 @@ if(navigator.geolocation)
             const {longitude}=position.coords;
             console.log(`https://www.google.com/maps/@{latitude},{longitude},15z?entry=ttu`);
             const coords=[latitude,longitude];
-            const map = L.map('map').setView(coords, 13);
+            map = L.map('map').setView(coords, 13);
 
             L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
                 attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
             }).addTo(map);
 
-            
-
-            map.on('click',function(mapEvent)
+            //handling clicks on map
+            map.on('click',function(mapE)
             {
-                console.log(mapEvent);
-
-                const {lat,lng}=mapEvent.latlng;
-
-                L.marker([lat,lng]).addTo(map)
-                .bindPopup(L.popup({
-                    maxWidth:250,
-                    minWidth:100,
-                    autoClose:false,
-                    closeOnClick:false,
-                    className:'running-popup',
-                }))
-                .setPopupContent('Workout')
-                .openPopup();
+                mapEvent=mapE;
+                form.classList.remove('hidden');
+                inputDistance.focus();
+                
             })
         },
 
@@ -52,3 +42,33 @@ if(navigator.geolocation)
             alert('Could not get your location');
     });
 }
+
+form.addEventListener('submit',function(e)
+{
+    e.preventDefault();
+    //clear input fields
+    inputDistance.value='';
+    inputDuration.value='';
+    inputCadence.value='';
+    inputElevation.value='';
+        console.log(mapEvent);
+
+        const {lat,lng}=mapEvent.latlng;
+
+        L.marker([lat,lng]).addTo(map)
+            .bindPopup(L.popup({
+            maxWidth:250,
+            minWidth:100,
+            autoClose:false,
+            closeOnClick:false,
+            className:'running-popup',
+        }))
+        .setPopupContent('Workout')
+        .openPopup();
+});
+
+inputType.addEventListener('change',function()
+{
+    inputElevation.closest('.form__row').classList.toggle('form__row--hidden'); //CLOSEST is like inverse query selector it will select parent rather than children
+    inputCadence.closest('.form__row').classList.toggle('form__row--hidden');
+});
